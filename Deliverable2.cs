c33a37d99c8284c510c88fd870be4764235d88bf
jacobboyle3 <101877999+jacobboyle3@users.noreply.github.com>
2022-03-20 09:41:49 -0400

    Update Deliverable2.cs

--- Deliverable2.cs
+++ Deliverable2.cs
@@ -1,18 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hi! Welcome to our buffet. All you can eat for $9.99! We only charge extra for coffee. How many people are in your group? Please, parties of 6 or lower.");
 
-    string People = (Console.ReadLine());
-    int NumberOfPeople;
-    bool success = int.TryParse(People, out NumberOfPeople);
-    bool valid = success && NumberOfPeople < 7;
-    decimal buffet = 9.99m;
-    decimal coffee = 2.00m;
-    decimal water = 0.00m;
-    int totalwater = 0;
-    int totalcoffee = 0;
+string People = (Console.ReadLine());
+int NumberOfPeople;
+bool success = int.TryParse(People, out NumberOfPeople);
+bool valid = success && NumberOfPeople < 7;
+decimal buffet = 9.99m;
+decimal coffee = 2.00m;
+decimal water = 0.00m;
+int totalwater = 0;
+int totalcoffee = 0;
 
-    decimal result1 = buffet + coffee;
-    decimal result2 = buffet + water;
+decimal result1 = buffet + coffee;
+decimal result2 = buffet + water;
 
 while (!valid)
 {
@@ -32,7 +32,7 @@ for (int i = 0; i < NumberOfPeople; i++)
     Console.WriteLine("person " + i + ", what would you like to drink? ");
     userInput = Console.ReadLine();
 
-    if (userInput !="water" && userInput != "coffee")
+    if (userInput != "water" && userInput != "coffee")
     { Console.WriteLine(userInput + " we don't have that. No drink for you!!! "); }
     else if (userInput == "water")
     {
@@ -40,15 +40,18 @@ for (int i = 0; i < NumberOfPeople; i++)
         totalwater++;
     }
     else
-    { Console.WriteLine("Coffee, ok!");
-    totalcoffee++;}
+    {
+        Console.WriteLine("Coffee, ok!");
+        totalcoffee++;
+    }
 
-    // if ((userInput == ("coffee")) || (userInput == ("water")))
-    //     { Console.WriteLine(userInput + ", ok! ");}
-    // else 
+
 
 }
 
-Console.WriteLine("Okay, so that is " + totalwater + "water(s) and " + totalcoffee + " coffee(s).");
+Console.WriteLine("Okay, so that is " + totalwater + " water(s) and " + totalcoffee + " coffee(s).");
+decimal totalBill = (decimal)NumberOfPeople * 9.99M + (decimal)totalwater * 0 + (decimal)totalcoffee * 2;
+Console.WriteLine($"Heres you check the total came out to be:  ${totalBill}");
+
 
 
